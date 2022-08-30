ff052a67cae0286d11e1e153176a835266eb16a6
yulin dima <yulin_dimon@mail.ru>
2022-08-30 23:49:06 +0300

    added all User,forgot Password,user Details,profile reducers

---
+++ src/store/reducers/user/profile-reducer.js
@@ -0,0 +1,71 @@
+import {
+  CLEAR_ERRORS,
+  DELETE_USER_FAIL,
+  DELETE_USER_REQUEST, DELETE_USER_RESET, DELETE_USER_SUCCESS, UPDATE_PASSWORD_FAIL,
+  UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET,
+  UPDATE_PROFILE_SUCCESS, UPDATE_USER_FAIL,
+  UPDATE_USER_REQUEST, UPDATE_USER_RESET, UPDATE_USER_SUCCESS
+} from "./constants";
+
+export const profileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_PROFILE_REQUEST:
+    case UPDATE_PASSWORD_REQUEST:
+    case UPDATE_USER_REQUEST:
+    case DELETE_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case UPDATE_PROFILE_SUCCESS:
+    case UPDATE_PASSWORD_SUCCESS:
+    case UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+
+    case DELETE_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isDeleted: action.payload.success,
+        message: action.payload.message,
+      };
+
+    case UPDATE_PROFILE_FAIL:
+    case UPDATE_PASSWORD_FAIL:
+    case UPDATE_USER_FAIL:
+    case DELETE_USER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case UPDATE_PROFILE_RESET:
+    case UPDATE_PASSWORD_RESET:
+    case UPDATE_USER_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
+
+    case DELETE_USER_RESET:
+      return {
+        ...state,
+        isDeleted: false,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
--- src/store/reducers/user/user-reducer.js
+++ src/store/reducers/user/user-reducer.js
@@ -6,27 +6,27 @@ import {
     REGISTER_USER_REQUEST,
     REGISTER_USER_SUCCESS,
     REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
 } from "./constants";
 
-const initialState = {
-    isAuth: false,
-    error: null,
-    loading: false,
-    user: {}
-}
 
-
-export const userReducer = (state = initialState, action) => {
+export const userReducer = (state = {user: {}}, action) => {
 
     switch (action.type) {
         case LOGIN_REQUEST:
         case REGISTER_USER_REQUEST:
+        case LOAD_USER_REQUEST:
             return {
                 loading: true,
                 isAuth: false,
             };
         case LOGIN_SUCCESS:
         case REGISTER_USER_SUCCESS:
+        case LOAD_USER_SUCCESS:
             return {
                 ...state,
                 loading: false,
@@ -34,6 +34,12 @@ export const userReducer = (state = initialState, action) => {
                 user: action.payload,
             };
 
+        case LOGOUT_SUCCESS:
+            return {
+                loading: false,
+                user: null,
+                isAuth: false,
+            };
         case LOGIN_FAIL:
         case REGISTER_USER_FAIL:
             return {
@@ -43,13 +49,29 @@ export const userReducer = (state = initialState, action) => {
                 user: null,
                 error: action.payload,
             };
+
+        case LOAD_USER_FAIL:
+            return {
+                loading: false,
+                isAuth: false,
+                user: null,
+                error: action.payload,
+            };
+
+        case LOGOUT_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+
         case CLEAR_ERRORS:
             return {
                 ...state,
                 error: null,
             };
-        default:
-            return state
-    }
 
+        default:
+            return state;
+    }
 }
