c919a176a52267162db81a1112a7a0fb86a26347
yulin dima <yulin_dimon@mail.ru>
2022-09-14 21:09:41 +0300

    fix

--- src/components/ui/UserOptions/UserOptions.jsx
+++ src/components/ui/UserOptions/UserOptions.jsx
@@ -11,93 +11,97 @@ import {Backdrop, SpeedDial, SpeedDialAction} from "@mui/material";
 import './UserOptions.scss'
 
 
-const styleSpeedDial = { zIndex: "11" }
-const styleBackdrop = { zIndex: "10" }
+const styleSpeedDial = {zIndex: "11"}
+const styleBackdrop = {zIndex: "10"}
 
 
-const UserOptions = ({ user }) => {
-  const { cartItems } = useSelector((state) => state.cart);
+const UserOptions = ({user}) => {
+    const {cartItems} = useSelector((state) => state.cart);
 
-  const [open, setOpen] = useState(false);
-  const history = useNavigate();
-  const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    const history = useNavigate();
+    const dispatch = useDispatch();
 
-  const options = [
-    { icon: <ListAltIcon />, name: "Заказы", func: orders },
-    { icon: <PersonIcon />, name: "Профиль", func: account },
-    {
-      icon: (
-        <ShoppingCartIcon
-          style={{ color: cartItems.length > 0 ? "tomato" : "unset" }}
-        />
-      ),
-      name: `Корзина(${cartItems.length})`,
-      func: cart,
-    },
-    { icon: <ExitToAppIcon />, name: "Выход", func: logoutUser },
-  ];
+    const options = [
+        {icon: <ListAltIcon/>, name: "Заказы", func: orders},
+        {icon: <PersonIcon/>, name: "Профиль", func: account},
+        {
+            icon: (
+                <ShoppingCartIcon
+                    style={{color: cartItems.length > 0 ? "tomato" : "unset"}}
+                />
+            ),
+            name: `Корзина(${cartItems.length})`,
+            func: cart,
+        },
+        {icon: <ExitToAppIcon/>, name: "Выход", func: logoutUser},
+    ];
 
-  if (user.role === "admin") {
-    options.unshift({
-      icon: <DashboardIcon />,
-      name: "Dashboard",
-      func: dashboard,
-    });
-  }
+    if (user.role === "admin") {
+        options.unshift({
+            icon: <DashboardIcon/>,
+            name: "Dashboard",
+            func: dashboard,
+        });
+    }
 
-  function dashboard() {
-    history.push("/admin/dashboard");
-  }
+    function dashboard() {
+        history.push("/admin/dashboard");
+    }
 
-  function orders() {
-    history.push("/orders");
-  }
-  function account() {
-    history.push("/account");
-  }
-  function cart() {
-    history.push("/cart");
-  }
-  function logoutUser() {
-    dispatch(userActions.logout());
-    alert.success("Вы успешно вышли из системы!");
-  }
+    function orders() {
+        history.push("/orders");
+    }
 
-  const onClickSetOpenHandler = ()=>{
-    setOpen(!open)
-  }
+    function account() {
+        history.push("/account");
+    }
 
-  return (
-    <>
-      <Backdrop open={open} style={styleBackdrop} />
-      <SpeedDial
-        ariaLabel="SpeedDial tooltip example"
-        onClose={onClickSetOpenHandler}
-        onOpen={onClickSetOpenHandler}
-        style={styleSpeedDial}
-        open={open}
-        direction="down"
-        className="speedDial"
-        icon={
-          <img
-            className="speedDialIcon"
-            src={user.avatar.url ? user.avatar.url : "/Profile.png"}
-            alt="Profile"
-          />
-        }
-      >
-        {options.map((item) => (
-          <SpeedDialAction
-            key={item.name}
-            icon={item.icon}
-            tooltipTitle={item.name}
-            onClick={item.func}
-            tooltipOpen={window.innerWidth <= 600 ? true : false}
-          />
-        ))}
-      </SpeedDial>
-    </>
-  );
+    function cart() {
+        history.push("/cart");
+    }
+
+    function logoutUser() {
+        dispatch(userActions.logout());
+        alert.success("Вы успешно вышли из системы!");
+    }
+
+    const onClickSetOpenHandler = () => {
+        setOpen(!open)
+    }
+    console.log('fd')
+
+    return (
+        <>
+            <Backdrop open={open} style={styleBackdrop}/>
+            <SpeedDial
+                ariaLabel="SpeedDial tooltip example"
+                onClose={onClickSetOpenHandler}
+                onOpen={onClickSetOpenHandler}
+                style={styleSpeedDial}
+                open={open}
+                direction="down"
+                className="speedDial"
+                icon={
+                    <img
+                        className="speedDialIcon"
+                        src={user.avatar.url ? user.avatar.url : "/Profile.png"}
+                        alt="Profile"
+                    />
+                }
+            >
+                {options.map((item) => (
+                    <SpeedDialAction
+                        key={item.name}
+                        icon={item.icon}
+                        tooltipTitle={item.name}
+                        onClick={item.func}
+                        tooltipOpen={window.innerWidth <= 600 ? true : false}
+                    />
+                ))}
+            </SpeedDial>
+        </>
+    );
 };
 
 export default UserOptions;
